22b2f8ea3230c5dfb337d00d226558d5e209cea7
GarrettMa <55817010+GarrettMa@users.noreply.github.com>
2020-01-06 14:26:01 -0800

    Importing drivetrain 2019 and teleop drive command

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,1 +1,25 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
+package frc.robot;
+
+import com.ctre.phoenix.ErrorCode;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+  public static void catchError(ErrorCode ec) {
+    if(ec != ErrorCode.OK) {
+      System.out.println("Error configuring in RobotMap.java at line: " + new Throwable().getStackTrace()[1].getLineNumber());
+      System.out.println(ec.toString());
+    }  
+  }
+}
