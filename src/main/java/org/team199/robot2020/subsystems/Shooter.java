cfcc95ed0ed58bdac017aa3e268063ab083c7a0e
Lilly He <lilly.he@yahoo.com>
2020-01-16 18:05:34 -0800

    Clarify file names, add shooter to RobotContainer

--- src/main/java/org/team199/robot2020/subsystems/Shooter.java
+++ src/main/java/org/team199/robot2020/subsystems/Shooter.java
@@ -1,8 +1,8 @@
 package org.team199.robot2020.subsystems;
 
-import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Shooter implements Subsystem {
+public class Shooter extends SubsystemBase {
     public static final double SHOOTING_SPEED = 0;
     private double targetSpeed;
     private double currentSpeed;
