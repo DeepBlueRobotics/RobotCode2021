1f8f8155452dfbdfd992382bc7a2e8bcc1aad5c4
anitabero <803264@seq.org>
2020-01-20 11:08:18 -0800

    added timer so data is only recorded by smartdashboard 1 time per second to fix cpu issue (hopefully)

--- src/main/java/org/team199/robot2020/commands/ShooterTargetSpeed.java
+++ src/main/java/org/team199/robot2020/commands/ShooterTargetSpeed.java
@@ -3,12 +3,19 @@ package org.team199.robot2020.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import org.team199.robot2020.subsystems.Shooter;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Timer;
 
 public class ShooterTargetSpeed extends CommandBase {
   Shooter shooter;
+  Timer timer;
 
   public ShooterTargetSpeed(Shooter shooter) {
     this.shooter = shooter;
+    timer = new Timer();
+  }
+
+  public void initialize(){
+    timer.start();
   }
 
   public void execute() {
@@ -19,7 +26,10 @@ public class ShooterTargetSpeed extends CommandBase {
     double speed = SmartDashboard.getNumber("Shooter Target Speed", 0); //makes Kevin #4 feel better
     shooter.setSetpoint(speed);
     SmartDashboard.putNumber("Shooter Distance", shooter.getCurrentDistance());
-    SmartDashboard.putNumber("Shooter Speed", java.lang.Math.round(shooter.getMeasurement()));
+
+    if ((timer.get() * 10) % 10 == 0){
+      SmartDashboard.putNumber("Shooter Speed", shooter.getMeasurement());
+    }
   }
 
   public boolean isFinished() {
