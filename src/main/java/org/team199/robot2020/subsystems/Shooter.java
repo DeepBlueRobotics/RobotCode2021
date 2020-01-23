7debe298cd82087c16ce48c9e44f0c795269772f
DriverStationComputer <deepbluerobotics@gmail.com>
2020-01-22 17:21:39 -0800

    reduced noise in velocity measurement

--- src/main/java/org/team199/robot2020/subsystems/Shooter.java
+++ src/main/java/org/team199/robot2020/subsystems/Shooter.java
@@ -5,6 +5,7 @@ import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.controller.PIDController;
 
 import org.team199.lib.MotorControllerFactory;
@@ -14,7 +15,7 @@ import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
 
 public class Shooter extends PIDSubsystem {
     private final WPI_VictorSPX flywheel = MotorControllerFactory.createVictor(Constants.Drive.FLYWHEEL_MOTOR);
-    private final Encoder encoder = new Encoder(new DigitalInput(4), new DigitalInput(5));
+    private final Encoder encoder = new Encoder(new DigitalInput(4), new DigitalInput(5), false, EncodingType.k1X );
     private final SimpleMotorFeedforward ff = new SimpleMotorFeedforward(-3.42857142857, 6.0/35);
     private double targetSpeed;
 
@@ -25,12 +26,13 @@ public class Shooter extends PIDSubsystem {
         SmartDashboard.putNumber("Shooter Target Speed", 0);
         SmartDashboard.putNumber("Shooter kP", 0);
         encoder.setDistancePerPulse(-1/8.75);
-        encoder.setSamplesToAverage(20);
+        encoder.setSamplesToAverage(24);
     }
 
     public void useOutput(double output, double setpoint) { // set flywheel speed
         flywheel.setVoltage(output + ff.calculate(output)); // TODO: add feedforward
     }
+    
 
     public double getMeasurement() { // get current speed
         return encoder.getRate();
