a8b92a64d26f2efa76a1430f326f6e7d04bc5a7f
DeepBlueRobotics <driverstation@carlmontrobotics.org>
2020-02-05 19:26:28 -0800

    fixed logging package names

--- src/main/java/org/team199/lib/logging/DataLog.java
+++ src/main/java/org/team199/lib/logging/DataLog.java
@@ -1,4 +1,4 @@
-package frc.robot.lib.logging;
+package org.team199.lib.logging;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
--- src/main/java/org/team199/lib/logging/LogFiles.java
+++ src/main/java/org/team199/lib/logging/LogFiles.java
@@ -1,4 +1,4 @@
-package frc.robot.lib.logging;
+package org.team199.lib.logging;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
