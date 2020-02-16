3d7940d08ef16380eec7f8de76b02cdf838deb63
Ryan DeBarger <806394@seq.org>
2020-02-15 16:47:03 -0800

    added error reporting code to motor configuration

--- src/main/java/org/team199/lib/MotorControllerFactory.java
+++ src/main/java/org/team199/lib/MotorControllerFactory.java
@@ -9,9 +9,11 @@ package org.team199.lib;
 
 import java.util.HashMap;
 
+import com.ctre.phoenix.ErrorCode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.revrobotics.CANError;
 import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
@@ -26,13 +28,15 @@ public class MotorControllerFactory {
   public static WPI_VictorSPX createVictor(int port) {
     WPI_VictorSPX victor = new WPI_VictorSPX(port);
 
+    ErrorCode[] errors = new ErrorCode[5];
     // Put all configurations for the victor motor controllers in here.
-    victor.configNominalOutputForward(0, 10);
-    victor.configNominalOutputReverse(0, 10);
-    victor.configPeakOutputForward(1, 10);
-    victor.configPeakOutputReverse(-1, 10);
-    victor.configNeutralDeadband(0.001, 10);
+    errors[0] = victor.configNominalOutputForward(0, 10);
+    errors[1] = victor.configNominalOutputReverse(0, 10);
+    errors[2] = victor.configPeakOutputForward(1, 10);
+    errors[3] = victor.configPeakOutputReverse(-1, 10);
+    errors[4] = victor.configNeutralDeadband(0.001, 10);
     victor.setNeutralMode(NeutralMode.Brake);
+    parseErrors("VictorSPX", port, errors, ErrorCode.OK);
 
     return victor;
   }
@@ -40,19 +44,21 @@ public class MotorControllerFactory {
   public static WPI_TalonSRX createTalon(int id) {
     WPI_TalonSRX talon = new WPI_TalonSRX(id);
 
+    ErrorCode[] errors = new ErrorCode[8];
     // Put all configurations for the talon motor controllers in here.
     // All values are from last year's code.
-    talon.configNominalOutputForward(0, 10);
-    talon.configNominalOutputReverse(0, 10);
-    talon.configPeakOutputForward(1, 10);
-    talon.configPeakOutputReverse(-1, 10);
-    talon.configPeakCurrentLimit(0, 0);
-    talon.configPeakCurrentDuration(0, 0);
+    errors[0] = talon.configNominalOutputForward(0, 10);
+    errors[1] = talon.configNominalOutputReverse(0, 10);
+    errors[2] = talon.configPeakOutputForward(1, 10);
+    errors[3] = talon.configPeakOutputReverse(-1, 10);
+    errors[4] = talon.configPeakCurrentLimit(0, 0);
+    errors[5] = talon.configPeakCurrentDuration(0, 0);
     // 40 Amps is the amp limit of a CIM. lThe PDP has 40 amp circuit breakers,
-    talon.configContinuousCurrentLimit(30, 0);
+    errors[6] = talon.configContinuousCurrentLimit(30, 0);
     talon.enableCurrentLimit(true);
-    talon.configNeutralDeadband(0.001, 10);
+    errors[7] = talon.configNeutralDeadband(0.001, 10);
     talon.setNeutralMode(NeutralMode.Brake);
+    parseErrors("TalonSRX", id, errors, ErrorCode.OK);
 
     return talon;
   }
@@ -82,20 +88,32 @@ public class MotorControllerFactory {
 
   public static CANSparkMax createSparkMax(int id) {
     CANSparkMax spark = new CANSparkMax(id, CANSparkMaxLowLevel.MotorType.kBrushless);
-    spark.restoreFactoryDefaults();
-    spark.setIdleMode(IdleMode.kBrake);
-    spark.enableVoltageCompensation(12);
-    spark.setSmartCurrentLimit(50);
+    CANError[] errors = new CANError[9];
+    errors[0] = spark.restoreFactoryDefaults();
+    errors[1] = spark.setIdleMode(IdleMode.kBrake);
+    errors[2] = spark.enableVoltageCompensation(12);
+    errors[3] = spark.setSmartCurrentLimit(50);
 
     checkSparkMaxErrors(spark);
 
     CANPIDController controller = spark.getPIDController();
-    controller.setOutputRange(-1, 1);
-    controller.setP(0);
-    controller.setI(0);
-    controller.setD(0);
-    controller.setFF(0);
+    errors[4] = controller.setOutputRange(-1, 1);
+    errors[5] = controller.setP(0);
+    errors[6] = controller.setI(0);
+    errors[7] = controller.setD(0);
+    errors[8] = controller.setFF(0);
+
+    parseErrors("SparkMax", id, errors, CANError.kOk);
 
     return spark;
+  }
+
+  private static <T extends Enum<T>> void parseErrors(String motorType, int id, T[] errors, T ok) {
+    for(T error: errors) {
+      if(error == null || error == ok) {
+        continue;
+      }
+      System.err.println("Error: " + error.name() + " occured while configuring: " + motorType + " on CAN port: " id);
+    }
   }
 }
