35098c01b64edb55433f4a5b14f0acceb4006d97
DeepBlueRobotics <driverstation@carlmontrobotics.org>
2020-02-26 18:25:18 -0800

    Add linear interpolation code to auto shoot and drive

--- src/main/java/org/team199/robot2020/commands/AutoShootAndDrive.java
+++ src/main/java/org/team199/robot2020/commands/AutoShootAndDrive.java
@@ -3,31 +3,33 @@ package org.team199.robot2020.commands;
 import com.playingwithfusion.TimeOfFlight;
 
 import org.team199.lib.Limelight;
+import org.team199.lib.LinearInterpolation;
 import org.team199.lib.RobotPath;
 import org.team199.robot2020.subsystems.Drivetrain;
 import org.team199.robot2020.subsystems.Feeder;
 import org.team199.robot2020.subsystems.Intake;
 import org.team199.robot2020.subsystems.Shooter;
 
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class AutoShootAndDrive extends SequentialCommandGroup {
-    public AutoShootAndDrive(Drivetrain drivetrain, Intake intake, Feeder feeder, Shooter shooter, Limelight lime, RobotPath path) {
+    public AutoShootAndDrive(Drivetrain drivetrain, Intake intake, Feeder feeder, Shooter shooter, 
+                             Limelight lime, RobotPath path, LinearInterpolation linearInterpol, Translation2d target) {
         addRequirements(drivetrain, intake, feeder, shooter);
 
         TimeOfFlight shooterDistanceSensor = feeder.getShooterDistanceSensor();
-        SequentialCommandGroup initShoot = new SequentialCommandGroup(
-            //new ShooterHorizontalAim(drivetrain, lime), 
-            new AutoShoot(feeder, shooter, shooterDistanceSensor, 3, 5500)
-        );
-        SequentialCommandGroup finalShoot = new SequentialCommandGroup(
-            //new ShooterHorizontalAim(drivetrain, lime), 
-            new AutoShoot(feeder, shooter, shooterDistanceSensor, 5, 4200)
-        );
+        ShooterHorizontalAim aim = new ShooterHorizontalAim(drivetrain, lime);
+        AutoShoot shoot = new AutoShoot(feeder, shooter, shooterDistanceSensor, 3);
 
         addCommands(
-            initShoot,
+            aim,
+            new InstantCommand(() -> { 
+                SmartDashboard.putNumber("Shooter.kTargetSpeed", linearInterpol.calculate(drivetrain.getOdometry().getPoseMeters().getTranslation().getDistance(target))); 
+            }),
+            shoot,
             new ShooterHorizontalAim(drivetrain, lime),
             new InstantCommand(() -> {
                 intake.intake();
@@ -38,7 +40,11 @@ public class AutoShootAndDrive extends SequentialCommandGroup {
                 intake.retract();
                 intake.stop();
             }, intake),
-            finalShoot
+            aim,
+            new InstantCommand(() -> { 
+                SmartDashboard.putNumber("Shooter.kTargetSpeed", linearInterpol.calculate(drivetrain.getOdometry().getPoseMeters().getTranslation().getDistance(target))); 
+            }),
+            shoot
         );
     }
 }
