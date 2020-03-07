9d1c431615432750c1988008d2eae8359b875023
Ryan DeBarger <806394@seq.org>
2020-03-06 18:22:57 -0800

    Revert "updated path switching code"
    This reverts commit 6c1926d59e18adf0443a9d38e349713b08cc1843.

--- src/main/java/org/team199/robot2020/Constants.java
+++ src/main/java/org/team199/robot2020/Constants.java
@@ -54,7 +54,6 @@ public final class Constants {
 
         public static final int kAutoPathSwitch1Port = 0;
         public static final int kAutoPathSwitch2Port = 1;
-        public static final int kAutoPathSwitch3Port = 2;
     }
 
     /**
