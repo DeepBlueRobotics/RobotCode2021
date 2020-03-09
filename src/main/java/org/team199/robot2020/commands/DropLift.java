3795178f175ce3e069ec9287c97b7b1c3d48eb99
DeepBlueRobotics <driverstation@carlmontrobotics.org>
2020-03-08 19:39:00 -0700

    Feeder and climber testing

--- src/main/java/org/team199/robot2020/commands/DropLift.java
+++ src/main/java/org/team199/robot2020/commands/DropLift.java
@@ -9,47 +9,40 @@ package org.team199.robot2020.commands;
 
 import org.team199.robot2020.subsystems.Climber;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class DeployClimber extends CommandBase {
+public class DropLift extends CommandBase {
   private final Climber climber;
 
-  /**
-   * Deploys climber up to switch
-   */
-  public DeployClimber(Climber climber) {
+  public DropLift(Climber climber) {
+    // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(this.climber = climber);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    climber.runLift(Climber.kLiftDeploySpeed);
-    climber.runWinch(Climber.kWinchDeploySpeed);
-    System.out.println("--RUNNING--");
+    climber.runLift(Climber.kLiftRetractSpeed);
   }
+      
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-      if (climber.getWinchHeight() >= 0) {
-        climber.runWinch(0);
-      }
+    if (climber.getLiftHeight() < 1) {
+      climber.runLift(0);
+    }
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    climber.runLift(Climber.kLiftKeepSpeed);
-    climber.runWinch(0);
-    System.out.println("--ENDED--");
+    climber.runLift(0);
   }
 
-  // eturns true when the command should end.
+  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return climber.getLiftHeight() >= Climber.kLiftHeight;
-   // return climber.getWinchHeight() >= 0;
+    return climber.getLiftHeight() <= 0;
   }
 }
--- src/main/java/org/team199/robot2020/subsystems/Climber.java
+++ src/main/java/org/team199/robot2020/subsystems/Climber.java
@@ -2,6 +2,7 @@ package org.team199.robot2020.subsystems;
 
 import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -10,30 +11,35 @@ import org.team199.lib.MotorControllerFactory;
 import org.team199.robot2020.Constants;
 
 public class Climber extends SubsystemBase {
-    private static final double kLiftConversionFactor = 43.98 / 256; // TODO: confirm numbers (inches)
-    private static final double kWinchConversionFactor = 26.39 / 256; // TODO: confirm numbers (inches)
+    private static final double kLiftConversionFactor = 6.0/40 * Math.PI * 3; // TODO: confirm numbers (inches)
+    private static final double kWinchConversionFactor = 1.0/9 * Math.PI; // TODO: confirm numbers (inches)
 
     // TODO: find good values and then set to final
-    public static double kLiftDeploySpeed = 0.9; // TODO: set correct speed
-    public static double kWinchDeploySpeed = 0.5; // TODO: set correct speed
-    public static double kLiftKeepSpeed = 0.2; // TODO: set correct speed
-    public static double kLiftRetractSpeed = -0.5; // TODO: set correct speed
-    public static double kWinchRetractSpeed = 0.8; // TODO: set correct speed
-    public static double kLiftAdjustSpeed = 0.1; // TODO: set correct speed
-    
-    public static final double kLiftHeight = 18.8;
-    public static final double kWinchEndHeight = 76.2;
-    public static final double kWinchStartHeight = -90;
+    public static double kLiftDeploySpeed = 0.3; // TODO: set correct speed
+    public static double kWinchDeploySpeed = 1; // TODO: set correct speed
+    public static double kLiftKeepSpeed = 0.06; // TODO: set correct speed
+    public static double kLiftRetractSpeed = -0.3; // TODO: set correct speed
+    public static double kWinchRetractSpeed = 0.6; // TODO: set correct speed
+    public static double kLiftAdjustSpeed = 0.2; // TODO: set correct speed
+    public static double kWinchAdjustSpeed = 0.2;
 
+    public static final double kLiftHeight = 87;
+    public static final double kWinchEndHeight = 30;
+    public static final double kWinchStartHeight = -80;
     private final CANSparkMax liftMotor = MotorControllerFactory.createSparkMax(Constants.Ports.kClimberLift);
     private final CANSparkMax winchMotor = MotorControllerFactory.createSparkMax(Constants.Ports.kClimberWinch);
-    private final CANEncoder liftEncoder = liftMotor.getEncoder();
-    private final CANEncoder winchEncoder = winchMotor.getEncoder();
+    private final CANEncoder liftEnc = liftMotor.getEncoder();
+    private final CANEncoder winchEnc = winchMotor.getEncoder();
 
     public Climber(){
-        liftEncoder.setPositionConversionFactor(kLiftConversionFactor);
-        winchEncoder.setPositionConversionFactor(kWinchConversionFactor);
-        winchEncoder.setPosition(kWinchStartHeight);
+        liftEnc.setPositionConversionFactor(kLiftConversionFactor);
+        winchEnc.setPositionConversionFactor(kWinchConversionFactor);
+        liftEnc.setVelocityConversionFactor(kLiftConversionFactor / 60);
+        winchEnc.setVelocityConversionFactor(kWinchConversionFactor / 60);
+        liftEnc.setPosition(0);
+        winchEnc.setPosition(kWinchStartHeight);
+        winchMotor.setIdleMode(IdleMode.kCoast);
+        liftMotor.setSmartCurrentLimit(30);
 
         SmartDashboard.putNumber("Climber.kLiftDeploySpeed", kLiftDeploySpeed);
         SmartDashboard.putNumber("Climber.kWinchDeploySpeed", kWinchDeploySpeed);
@@ -50,21 +56,38 @@ public class Climber extends SubsystemBase {
         kLiftRetractSpeed = SmartDashboard.getNumber("Climber.kLiftRetractSpeed", kLiftRetractSpeed);
         kWinchRetractSpeed = SmartDashboard.getNumber("Climber.kWinchRetractSpeed", kWinchRetractSpeed);
         kLiftAdjustSpeed = SmartDashboard.getNumber("Climber.kLiftAdjustSpeed", kLiftAdjustSpeed);
+        SmartDashboard.putNumber("Lift Position", getLiftHeight());
+        SmartDashboard.putNumber("Winch Position", getWinchHeight());
+        SmartDashboard.putNumber("Lift Speed", getLiftSpeed());
+        SmartDashboard.putNumber("Winch Speed", getWinchSpeed());
     }
 
     public void runLift(double speed) {
+        System.out.println("CARRY ME BOII IS SPEED " + liftMotor.getEncoder().getVelocity());
         liftMotor.set(speed);
+        if (liftMotor.getEncoder().getVelocity() == 0){
+            System.out.println("UNWIND WHEN DONE TESTING!!!");
+        }
     }
 
     public void runWinch(double speed) {
+        System.out.println("HEYY WINCH IS RUNNING AT SPEED " + winchMotor.getEncoder().getVelocity());
         winchMotor.set(Math.abs(speed));
     }
 
     public double getLiftHeight() {
-        return liftEncoder.getPosition();
+        return liftEnc.getPosition();
     }
 
     public double getWinchHeight() {
-        return winchEncoder.getPosition();
+        return winchEnc.getPosition();
+    }
+
+    public double getLiftSpeed() {
+        return liftEnc.getVelocity();
+    }
+
+    public double getWinchSpeed() {
+        return winchEnc.getVelocity();
     }
 }
