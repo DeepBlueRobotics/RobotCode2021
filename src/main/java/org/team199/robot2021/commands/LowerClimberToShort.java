2e98be80db38d5bfa1f9cd805211978f8f615427
kaseyliu <mermaidmagic99@gmail.com>
2021-05-14 20:46:13 -0700

    change DeployClimberShort to LowerClimberToShort

--- src/main/java/org/team199/robot2021/commands/LowerClimberToShort.java
+++ src/main/java/org/team199/robot2021/commands/LowerClimberToShort.java
@@ -5,14 +5,14 @@ import org.team199.robot2021.subsystems.Climber;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class DeployClimberShort extends CommandBase {
+public class LowerClimberToShort extends CommandBase {
   private final Climber climber;
 
   /**
    * Deploys climber up to switch
    * Old needs to be updated to current code
    */
-  public DeployClimberShort(Climber climber) {
+  public LowerClimberToShort(Climber climber) {
     addRequirements(this.climber = climber);
   }
 
@@ -20,7 +20,7 @@ public class DeployClimberShort extends CommandBase {
   @Override
   public void initialize() {
     //Sends arm up to bar and gives slack from winch to do so
-    climber.runLift(Climber.kLiftDeploySpeed);
+    climber.runLift(Climber.kLiftLowerSpeed);
     //Puts winch into idle mode (coasting) so the arm can lift it
     climber.setWinchIdleCoast();
     System.out.println("--RUNNING--");
@@ -29,11 +29,7 @@ public class DeployClimberShort extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-      //In case the winch somehow gets faster than the arm and needs to be stopped before it extends farther than the max
-      //this probably won't be necessary thx to equations :)
-      if (climber.getWinchHeight() >= Climber.kWinchMaxHeight) {
-        climber.runWinch(0);
-      }
+
   }
 
   // Called once the command ends or is interrupted.
@@ -41,15 +37,13 @@ public class DeployClimberShort extends CommandBase {
   public void end(boolean interrupted) {
     //When it reaches the top this stops all of it
     climber.runLift(0);
-    //Sets winch to break mode so it doesn't spin freely after arm deploys
-    climber.setWinchIdleBrake();
     System.out.println("--ENDED--");
   }
 
   // returns true when the command should end.
   @Override
   public boolean isFinished() {
-    //Checks if arm has reached top
-    return climber.getLiftHeight() >= Climber.kLiftShortHeight;
+    //Checks if arm has lowered to shorter height from being at the top
+    return climber.getLiftHeight() <= Climber.kLiftShortHeight;
   }
 }
