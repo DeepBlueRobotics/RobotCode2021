4b6eeb4ed6d87ac78a345048385e9e3dacb23c9b
0sani <jackahansen18@gmail.com>
2021-09-10 15:54:35 -0700

    incorportated BallSearchAndPickup button

--- src/main/java/org/team199/robot2021/commands/BallSearchAndPickup.java
+++ src/main/java/org/team199/robot2021/commands/BallSearchAndPickup.java
@@ -47,8 +47,8 @@ public class BallSearchAndPickup extends SequentialCommandGroup {
     DriveDistance drive = new DriveDistance(drivetrain, distance);
     //Rotate Robot along angle till it's facing ball
       //Create new command to do this where isfinished checks if the goal angle is the same as the current angle
-    //Run the intak
-    addCommands(rotate, new InstantCommand(() -> intake.deploy()), new InstantCommand(() -> intake.intake()), drive);
+    //Run the intake
+    addCommands(rotate, new InstantCommand(() -> intake.deploy()), new InstantCommand(() -> intake.intake()), drive, new InstantCommand(() -> intake.stop()), new InstantCommand(() -> intake.retract()));
   }
 
   // Called when the command is initially scheduled.
